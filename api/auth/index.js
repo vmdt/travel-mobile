e32cdf181bd1f975be7c394d29e7cddcaaa625bb
vmdt <vmdt03@gmail.com>
2024-09-18 03:22:22 +0700

    [chore]: mock data for app running

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,9 +1,28 @@
 import { StatusCodes } from "http-status-codes";
 import { API_ENDPOINTS } from "../../constants/api";
+import { mockUserLogin, mockUserSignup } from "../../data/user";
 import { api } from "../axios";
 
 export const login = async (email, password) => {
 	try {
+		// This is mock data to ignore the actual API calling
+		if (
+			email === mockUserLogin.metadata.user.email &&
+			password === mockUserLogin.metadata.user.password
+		) {
+			return mockUserLogin;
+		} else if (
+			email === mockUserSignup.metadata.user.email &&
+			password === mockUserSignup.metadata.user.password
+		) {
+			return mockUserSignup;
+		} else {
+			return {
+				code: StatusCodes.BAD_REQUEST,
+				message: "Invalid email or password",
+			};
+		}
+
 		const response = await api.post(API_ENDPOINTS.LOGIN, {
 			email,
 			password,
@@ -18,7 +37,18 @@ export const login = async (email, password) => {
 };
 
 export const signup = async (data) => {
+	const isMock = true;
 	try {
+		// This is mock data to ignore the actual API calling
+		if (isMock) {
+			mockUserSignup.metadata.user = {
+				...data,
+				profilePicture:
+					"https://res.cloudinary.com/dxrygyw5d/image/upload/v1713621780/travelife/user/65e37174b0e7527a1ad579b6.jpg",
+			};
+			return mockUserSignup;
+		}
+
 		const response = await api.post(API_ENDPOINTS.SIGNUP, data);
 		return response.data;
 	} catch (error) {
---
+++ data/user.js
@@ -0,0 +1,21 @@
+const mockUserLogin = {
+	metadata: {
+		user: {
+			username: "datvm",
+			email: "datvm@gmail.com",
+			password: "12345678",
+			fullname: "Vo Minh Dat",
+			dateOfBirth: "2003-09-05",
+			phone: "123456789",
+			address: "HCMC",
+			profilePicture:
+				"https://res.cloudinary.com/dxrygyw5d/image/upload/v1713621780/travelife/user/65e37174b0e7527a1ad579b6.jpg",
+		},
+	},
+};
+
+let mockUserSignup = {
+	metadata: {},
+};
+
+export { mockUserLogin, mockUserSignup };
